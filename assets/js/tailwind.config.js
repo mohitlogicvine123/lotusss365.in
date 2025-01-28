efb25a0481daf77710a0c05e834fc2154147a63f
mohitlogicvine123 <mohit.logicvine@gmail.com>
2025-01-28 18:56:10 +0530

    s

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'opensans': ['"Open Sans", sans-serif '],
+        },
+        extend: {
+            colors: {
+                theme1: '#0c9971',
+                theme2: '#f9df10',
+            },
+        },
+    },
+};
